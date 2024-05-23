fa7e117a65918e4fc28897b70a6dee6f41d3c01f
HarshaBordhan <100533487+HarshaBordhan@users.noreply.github.com>
2024-05-23 17:51:53 +0600

    set up sanity studio and schemas

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,9 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import { post } from "./schemas/post";
+import { author } from "./schemas/author";
+import { category } from "./schemas/category";
+import { blockContent } from "./schemas/blockContent";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [post, author, category, blockContent],
+};
---
+++ sanity/schemas/post.ts
@@ -0,0 +1,261 @@
+interface Selection {
+  title?: string;
+  author?: string;
+  media?: string;
+}
+
+export const post = {
+  name: "post",
+  title: "Post",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (rule: any) => rule.required(),
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+        isUnique: (value: string, context: any) =>
+          context.defaultIsUnique(value, context),
+      },
+      validation: (rule: any) => rule.required(),
+    },
+
+    {
+      name: "thumbnail",
+      title: "Thumbnail",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    },
+    {
+      name: "introImage",
+      title: "Intro image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    },
+    {
+      name: "projectIntro",
+      title: "Project intro",
+      type: "string",
+    },
+    {
+      name: "firstImage",
+      title: "First image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    },
+    {
+      name: "projectGoal",
+      title: "Project goal",
+      type: "string",
+    },
+    {
+      name: "secondImage",
+      title: "Second image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    },
+    {
+      name: "projectSolution",
+      title: "Project solution",
+      type: "string",
+    },
+    {
+      name: "solutions",
+      title: "Solutions",
+      type: "array",
+      of: [{ type: "image", options: { hotspot: true } }],
+    },
+    {
+      name: "thirdImage",
+      title: "Third Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    },
+    {
+      name: "fourthImage",
+      title: "Fourth image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    },
+    {
+      name: "fifthImage",
+      title: "Fifth image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    },
+    {
+      name: "sixthImage",
+      title: "Sixth image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    },
+    {
+      name: "seventhImage",
+      title: "Seventh image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    },
+    {
+      name: "eighthImage",
+      title: "Eighth image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative Text",
+        },
+      ],
+    },
+    {
+      name: "figmaProjectLink",
+      title: "Figma project link",
+      type: "url",
+      validation: (Rule: any) =>
+        Rule.uri({
+          scheme: ["http", "https", "mailto", "tel"],
+        }),
+    },
+    {
+      name: "projectYear",
+      title: "Project year",
+      type: "string",
+    },
+    {
+      name: "liveLink",
+      title: "Live link",
+      type: "url",
+      validation: (Rule: any) =>
+        Rule.uri({
+          scheme: ["http", "https", "mailto", "tel"],
+        }),
+    },
+
+    {
+      name: "author",
+      title: "Author",
+      type: "reference",
+      to: { type: "author" },
+    },
+    {
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [{ type: "reference", to: { type: "category" } }],
+    },
+    {
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
+    },
+    {
+      name: "body",
+      title: "Body",
+      type: "blockContent",
+    },
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      author: "author.name",
+      media: "mainImage",
+    },
+    prepare(selection: Selection) {
+      const { author } = selection;
+      return { ...selection, subtitle: author && `by ${author}` };
+    },
+  },
+};
