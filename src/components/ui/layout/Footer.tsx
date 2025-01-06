ca0f7bec31ae86e7b2ac2d032071538702b0cdb8
HarshaBordhan <harshabordhanc@gmail.com>
2025-01-06 17:18:13 +0600

    feat: added twitter and linkedin link

--- src/components/ui/layout/Footer.tsx
+++ src/components/ui/layout/Footer.tsx
@@ -1,4 +1,6 @@
 import SmallContainer from "@/components/ui/layout/SmallContainer";
+import Link from "next/link";
+import { Linkedin, Twitter } from "lucide-react";
 
 export default function Footer() {
   let currentYear = new Date().getFullYear();
@@ -6,7 +8,19 @@ export default function Footer() {
   return (
     <section>
       <SmallContainer>
-        <div className="flex flex-col justify-center items-center py-32 sm:gap-12">
+        <div className="flex flex-col justify-center items-center py-32 gap-8 sm:gap-12">
+          <div className="flex gap-4">
+            <Link href="https://x.com/thatpollen" legacyBehavior>
+              <a target="_blank">
+                <Twitter size={16} />
+              </a>
+            </Link>
+            <Link href="https://www.linkedin.com/in/thatpollen/" legacyBehavior>
+              <a target="_blank">
+                <Linkedin size={16} />
+              </a>
+            </Link>
+          </div>
           <p className="text-xs leading-[1.5] font-light">
             All works by Pollen @{currentYear}
           </p>
