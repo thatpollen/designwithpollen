3612d2346c277cc3afbed6d397d67c0a8a3da9dc
Pollen <talkspollen@gmail.com>
2025-03-18 17:18:09 +0600

    Update

--- src/devprojects/framer-projects.ts
+++ src/devprojects/framer-projects.ts
@@ -22,7 +22,7 @@ export const framerProjects: FramerProjectProps[] = [
     previewUrl: "https://weekly-shares-433142.framer.app",
   },
   {
-    name: "Language Resources",
+    name: "Learn Irish",
     projectYear: "2025",
     mp4Source: "/mp4/framer-3.mp4",
     url: "",
