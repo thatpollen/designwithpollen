75378611ee2248a9513247564cf41d4583ac6606
Pollen <talkspollen@gmail.com>
2025-03-18 17:08:18 +0600

    Update

--- src/devprojects/framer-projects.ts
+++ src/devprojects/framer-projects.ts
@@ -22,7 +22,7 @@ export const framerProjects: FramerProjectProps[] = [
     previewUrl: "https://weekly-shares-433142.framer.app",
   },
   {
-    name: "SeoGaeilge – Irish Language",
+    name: "SeoGaeilge Irish Language Resources",
     projectYear: "2025",
     mp4Source: "/mp4/framer-3.mp4",
     url: "",
